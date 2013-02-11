178b11142a1ba2c183df7668112c853a7d37e912
Eric Dorsey <eric@wearelimitless.com>
2013-02-10 22:11:04 -0600

    Turn Monitor on and off

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ app.get('/wakeup.html', function(req, res) {
 	res.end();
 });
 
+app.get('/sleep.html', function(req, res) {
+	exec('xset dpms force off\n', function(err, out, stderr) {
+		console.log(err, out, stderr);
+	});
+	res.end();
+});
+
 app.listen(3000);
 console.log('Listening on port 3000');
 
