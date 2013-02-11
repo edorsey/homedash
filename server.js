510036d7bc3647482b62ae6edd9e53df9d97fbee
Eric Dorsey <eric@wearelimitless.com>
2013-02-10 21:47:33 -0600

    Turn on now that we know it works

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var exec = require('child_process').exec;
 app.use(express.static(__dirname + '/public'));
 
 app.get('/wakeup.html', function(req, res) {
-	exec('xset dpms force off\n', function(err, out, stderr) {
+	exec('xset dpms force on\n', function(err, out, stderr) {
 		console.log(err, out, stderr);
 	});
 	res.end();
