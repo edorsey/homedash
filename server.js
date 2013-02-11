857c66441e3b84256b52d4fe92cb28865a15ddd0
Eric Dorsey <eric@wearelimitless.com>
2013-02-10 21:33:33 -0600

    Initial Comiit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var terminal = require('child_process').spawn('cmd');
+
+terminal.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+});
+
+terminal.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+});
+
+terminal.on('exit', function (code) {
+    console.log('child process exited with code ' + code);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/wakeup.html', function(req, res) {
+	terminal.stdin.write('nircmd monitor on\n');
+	res.end();
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
+
